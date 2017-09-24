99016cd8fac076f6856422666e961ecc5e2b6a69
Parker Catalano <catalano.work@gmail.com>
2017-09-24 18:14:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var a, b
-
-a = Math.floor(Math.random() * 1000)
-b = Math.floor(Math.random() * 1000)
-
 function add() {
+  var a, b
+
+  a = Math.floor(Math.random() * 1000)
+  b = Math.floor(Math.random() * 1000)
+  
   return a + b
 }
